db0e8bf232ac8c247d9a57bc417d956b77851774
calro999 <gamingmedia.mail@gmail.com>
2025-05-25 15:56:00 +0700

    セーブポイント2

---
+++ data/articles.ts
@@ -0,0 +1,30 @@
+const articles = [
+    {
+      id: 1,
+      title: "1芸能界に新たなカップル誕生？熱愛報道の真相に迫る",
+      description: "人気俳優と女優の熱愛が報じられ、ファンの間で大きな話題に。関係者によると二人の交際は…",
+      image: "/sample.jpg",
+      category: "芸能",
+      readTime: "2分で読める",
+    },
+    {
+      id: 2,
+      title: "2話題の新曲がYouTubeで1000万再生突破！",
+      description: "SNSで話題になっている人気アーティストの最新シングルが、再生回数で新記録を樹立。",
+      image: "/sample.jpg",
+      category: "音楽",
+      readTime: "3分で読める",
+    },
+    {
+        id: 3,
+        title: "3話題の新曲がYouTubeで1000万再生突破！",
+        description: "SNSで話題になっている人気アーティストの最新シングルが、再生回数で新記録を樹立。",
+        image: "/sample.jpg",
+        category: "音楽",
+        readTime: "3分で読める",
+      },
+    // 必要に応じて追加
+  ]
+  
+  export default articles
+  
